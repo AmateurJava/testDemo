f2c8dcbe665373af00e1805046be8a07ab864754
AmateurJava <liangjyft@163.com>
2018-12-16 23:20:20 +0800

    dd

--- src/main/java/com/testDemo/App.java
+++ src/main/java/com/testDemo/App.java
@@ -6,6 +6,6 @@ package com.testDemo;
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" ); 
     }
 }
